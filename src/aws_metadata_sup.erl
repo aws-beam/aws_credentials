c4dde8ec100d15a1b45aacb1f488c41caac3ce97
Jamu Kakar <jkakar@kakar.ca>
2015-10-29 21:59:51 -0700

    Port and slightly refactor the logic from Liz Frost's aws-erlang branch that adds support for AWS metadata.

--- src/aws_metadata_sup.erl
+++ src/aws_metadata_sup.erl
@@ -28,7 +28,14 @@ start_link() ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
+    {ok, {{one_for_all, 0, 1},
+          [{aws_metadata, {aws_metadata, start_link, []},
+            permanent,
+            5000,
+            worker,
+            [aws_metadata]}]
+         }
+    }.
 
 %%====================================================================
 %% Internal functions
