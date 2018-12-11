cb25351398cc6268a4c4b4874edc9a5603ce9daa
Mark Allen <mrallen1@yahoo.com>
2018-12-11 17:47:43 -0600

    Add a retry

--- src/aws_credentials_httpc.erl
+++ src/aws_credentials_httpc.erl
@@ -4,23 +4,41 @@
 %% `{error, Reason :: term()}'.
 %% @end
 -module(aws_credentials_httpc).
--export([start/0, get/1]).
+-export([start/0, get/1, get/2]).
 
 -define(PROFILE, aws_credentials).
 -define(TIMEOUT, 10000). % 10 sec
 -define(CONNECT_TIMEOUT, 3000). % 3 sec
+-define(BASE, 4).
+-define(DEFAULT_RETRIES, 3).
+-define(DELAY, 100). % 100 microseconds
 
 start() ->
     inets:start(httpc, [{profile, ?PROFILE}]).
-
 
 -spec get( URL :: binary() ) -> {ok, { Status :: non_neg_integer(),
                                        Body :: binary(),
                                        Headers :: map() } } | {error, Reason :: term()}.
 get(URL) ->
-    {ok, {{_HttpVer, Status, _Reason}, Body, Headers}} =
+  get(URL, ?DEFAULT_RETRIES).
+
+-spec get( URL :: binary(),
+           Retries :: pos_integer() ) -> {ok, { Status :: non_neg_integer(),
+                                                Body :: binary(),
+                                                Headers :: map() } } | {error, Reason :: term()}.
+get(_URL, 0) -> {error, no_retries};
+get(URL, Retries) ->
+    case make_request(URL) of
+      {ok, {{_HttpVer, Status, _Reason}, Body, Headers}} -> {ok, Status, Body, maps:from_list(Headers)};
+      Error -> 
+        error_logger:error_msg("Error fetching URL (retry attempts remaining: ~p) ~p: ~p.",
+                               [Retries, URL, Error]), 
+        timer:sleep((?BASE - Retries)*?DELAY),
+        get(URL, Retries - 1)
+    end.
+
+make_request(URL) ->
          httpc:request(get, {URL, []},
                        [{timeout, ?TIMEOUT}, {connect_timeout, ?CONNECT_TIMEOUT}], % HTTP options
                        [{body_format, binary}], % options
-                       ?PROFILE),
-    {ok, Status, Body, maps:from_list(Headers)}.
+                       ?PROFILE).
