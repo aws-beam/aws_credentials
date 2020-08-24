11dc37881bec7bc2ef9095a9fe2e058c1e59f980
Yang Sun <yang.sun@klarna.com>
2020-08-24 11:47:27 +0200

    maps:from_list doesn't work with Headers.

--- src/aws_credentials_httpc.erl
+++ src/aws_credentials_httpc.erl
@@ -46,7 +46,7 @@ get(_URL, _Tries, 0, Errs) -> {error, lists:reverse(Errs)};
 get(URL, Tries, Remaining, Errs) ->
     case make_request(URL) of
       {ok, {{_HttpVer, Status, _Reason}, Body, Headers}} ->
-        {ok, Status, Body, maps:from_list(Headers)};
+        {ok, Status, Body, Headers};
 
       Error ->
         NewRemaining = Remaining - 1,
