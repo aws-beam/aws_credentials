19ff9978b60fd5395b8fb884d0475b6e3c270f6c
Gwendolyn Van Hove <gwendolyn@mechanical-orchard.com>
2025-07-18 14:02:35 -0700

    Fix formatting and dialyzer issues

--- src/aws_credentials_eks.erl
+++ src/aws_credentials_eks.erl
@@ -7,9 +7,9 @@
 
 -export([fetch/1]).
 
--spec fetch(any()) ->
-        {ok, aws_credentials:credentials(), aws_credentials_provider:expiration()} |
-        {error, any()}.
+-spec fetch(aws_credentials_provider:options()) ->
+        {error, _}
+      | {ok, aws_credentials:credentials(), aws_credentials_provider:expiration()}.
 fetch(_Options) ->
   FullUri = os:getenv("AWS_CONTAINER_CREDENTIALS_FULL_URI"),
   TokenFile = os:getenv("AWS_CONTAINER_AUTHORIZATION_TOKEN_FILE"),
@@ -17,24 +17,27 @@ fetch(_Options) ->
   Response = fetch_session_token(FullUri, AuthToken),
   make_map(Response).
 
--spec read_token(false | string()) -> {ok, string()} | {error, any()}.
+-spec read_token(false | string()) -> {error, _} | {ok, binary()}.
 read_token(false) -> {error, no_credentials};
 read_token(Path) -> file:read_file(Path).
 
--spec fetch_session_token(false | string(), {error, any()} | {ok, string()}) ->
-	{error, any()} | {ok, aws_credentials_httpc:status_code(),
-                            aws_credentials_httpc:body(),
-                            aws_credentials_httpc:headers()}.
+-spec fetch_session_token(false | string(), {error, _} | {ok, string()}) ->
+        {error, _}
+      | {ok, aws_credentials_httpc:status_code(),
+              aws_credentials_httpc:body(),
+              aws_credentials_httpc:headers()}.
 fetch_session_token(false, _AuthToken) -> {error, no_credentials};
 fetch_session_token(_FullUri, {error, _Error} = Error) -> Error;
 fetch_session_token(FullUri, {ok, AuthToken}) ->
   aws_credentials_httpc:request(get, FullUri, [{?AUTHORIZATION_HEADER, AuthToken}]).
 
 
--spec make_map({error, any()} | {ok, aws_credentials_httpc:status_code(),
-                            aws_credentials_httpc:body(),
-                            aws_credentials_httpc:headers()}) ->
-	{error, any()} | {ok, aws_credentials:credentials(), aws_credentials_provider:expiration()}.
+-spec make_map({error, _}
+             | {ok, aws_credentials_httpc:status_code(),
+                    aws_credentials_httpc:body(),
+                    aws_credentials_httpc:headers()}) ->
+        {error, _}
+      | {ok, aws_credentials:credentials(), aws_credentials_provider:expiration()}.
 make_map({error, _Error} = Error) -> Error;
 make_map({ok, _Status, Body, _Headers}) ->
   #{ <<"AccessKeyId">> := AccessKeyId
